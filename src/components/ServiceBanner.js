95817105c962523813b30ddc1ed7b3d955e635b5
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-08-20 11:24:33 +0530

    Minor Changes

--- src/components/ServiceBanner.js
+++ src/components/ServiceBanner.js
@@ -4,8 +4,8 @@ const ServiceBanner = () => {
   return (
     <>
       <div className="h-96 text-white bg-[#202023] grid place-content-center eve">
-        <h1 className="text-3xl md:leading-[70px] md:text-5xl font-bold text-[#ffc800] text-center font-serif w-8/12 mx-auto p-4 bg-[#2a201fb0] rounded-2xl ">
-          "We strive for anything you need to make your home your own"
+        <h1 className="text-2xl md:leading-[70px] md:text-4xl font-bold text-[#ffc800] text-center font-serif w-8/12 mx-auto p-4 bg-[#2a201fb0] rounded-2xl ">
+          "We strive for anything you need to make home your own with the best design & execution"
         </h1>
       </div>
     </>
