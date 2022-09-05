81cd498c99167250738c908d78745933ebde1562
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-09-05 13:20:24 +0530

    Testimonials

---
+++ src/data/TestimonialsData.js
@@ -0,0 +1,17 @@
+import maleplace from '../assets/testimonials/maleplace.jpeg'
+import feplace from '../assets/testimonials/feplace.jpeg'
+
+const TestData = [
+    {
+        name:"Amruta Pande",
+        rev:"We did interior design and execution for 2 homes with HomeDez, one apartment and one villa within 5 years. We were very impressed by HomeDez design finish when we gave them the first work. HomeDez team did a great job with our apartment making it the best apartment in our society. Recently we had a very beautiful design but complex to execute for our villa, but HomeDez really did an amazing job to execute it to perfection with the best quality and most importantly within our cost range. It came out so good that it stands out in our society. More importantly, HomeDez is very good at after work service as long as you need it.",
+        img:feplace
+    },
+    {
+        name:"Vikas & Kavitha",
+        rev:"HomeDez team provides interior support and timely help given at the right cost and time. We have multiple choices with the perfection and satisfaction. We had a nice rapport with the team which sachin interior makes sure from their end!! Thanks Kolangi for your support, great work!!! They provide quality product at affordable rates and delivery is on time.",
+        img:maleplace
+    },
+]
+
+export default TestData;
--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -1,134 +1,186 @@
-import React from "react";
-import place from "../assets/place.jpeg"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+import TestData from "../data/TestimonialsData";
+
+const PreviousBtn = (props) => {
+  console.log(props);
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowBackIos style={{ color: "gray", fontSize: "45px" }} />
+    </div>
+  );
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowForwardIos style={{ color: "gray", fontSize: "45px" }} />
+    </div>
+  );
+};
 
 const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
   return (
-    <>
-      <div id="tests" className="text-center bg-slate-200 py-14 md:py-20">
-        <h1 data-aos="fade-up" className="text-6xl">Customer Testimonials</h1>
-        <div className="px-2 md:px-0 md:w-7/12 mx-auto pt-14">
-          <div
-            id="carouselExampleCaptions"
-            className="carousel slide relative carousel-dark"
-            data-bs-ride="carousel"
+    <div id="tests" className="text-center bg-slate-200 py-14 md:py-20">
+      <h1 data-aos="fade-up" className="text-6xl">
+        Customer Testimonials
+      </h1>
+      <div
+        className="testimonial"
+        style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+      >
+        <div className="md:w-1/2 w-11/12 text-center">
+          <Slider
+            prevArrow={<PreviousBtn />}
+            nextArrow={<NextBtn />}
+            {...settings}
           >
-            <div className="carousel-inner relative w-full overflow-hidden">
-              <div className="carousel-item active relative float-left w-full">
-                <img
-                data-aos="fade-up"
-                  className="rounded-full shadow-lg mb-6 mx-auto"
-                  src={place}
-                  alt="avatar"
-                  style={{ width: "150px" }}
-                />
-                <div data-aos="fade-up" className="flex flex-wrap justify-center">
-                  <div className="grow-0 shrink-0 basis-auto w-full lg:w-8/12 px-3">
-                    <h5 className="text-4xl font-bold mb-3 ">Amruta Pande</h5>
-                    <p className="text-gray-500 mb-6 md:text-xl text-lg">
-                      <svg
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fas"
-                        data-icon="quote-left"
-                        className="w-14 pr-2 inline-block"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
-                        ></path>
-                      </svg>
-                      <br/>We did interior design and execution for 2 homes with HomeDez, one apartment and one villa within 5 years. We were very impressed by HomeDez design finish when we gave them the first work. HomeDez team did a great job with our apartment making it the best apartment in our society. Recently we had a very beautiful design but complex to execute for our villa, but HomeDez really did an amazing job to execute it to perfection with the best quality and most importantly within our cost range. It came out so good that it stands out in our society. More importantly, HomeDez is very good at after work service as long as you need it.
-                    </p>
-                    <ul className="flex justify-center mb-0">
-                      <li>
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="star"
-                          className="w-4 text-yellow-500"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 576 512"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                          ></path>
-                        </svg>
-                      </li>
-                      <li>
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="star"
-                          className="w-4 text-yellow-500"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 576 512"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                          ></path>
-                        </svg>
-                      </li>
-                      <li>
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="star"
-                          className="w-4 text-yellow-500"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 576 512"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                          ></path>
-                        </svg>
-                      </li>
-                      <li>
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="star"
-                          className="w-4 text-yellow-500"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 576 512"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                          ></path>
-                        </svg>
-                      </li>
-                      <li>
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="star"
-                          className="w-4 text-yellow-500"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 576 512"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                          ></path>
-                        </svg>
-                      </li>
-                      {/* <li>
+            {TestData.map((dat, i) => {
+              return <TestCard data={dat} key={i} />;
+            })}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TestCard = (props) => {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide relative carousel-dark"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner relative w-full overflow-hidden">
+        <div className="carousel-item active relative float-left w-full">
+          <img
+            data-aos="fade-up"
+            className="rounded-full shadow-lg mb-6 mx-auto"
+            src={props.data.img}
+            alt="avatar"
+            style={{ width: "150px" }}
+          />
+          <div data-aos="fade-up" className="flex flex-wrap justify-center">
+            <div className="grow-0 shrink-0 basis-auto w-full px-3">
+              <h5 className="text-4xl font-bold mb-3 ">{props.data.name}</h5>
+              <p className="text-gray-500 mb-6 md:text-xl text-base">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="quote-left"
+                  className="w-14 pr-2 inline-block"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                  ></path>
+                </svg>
+                <br />
+                {props.data.rev}
+              </p>
+              <ul className="flex justify-center mb-0">
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-4 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-4 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-4 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-4 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="star"
+                    className="w-4 text-yellow-500"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                    ></path>
+                  </svg>
+                </li>
+                {/* <li>
                         <svg
                           aria-hidden="true"
                           focusable="false"
@@ -145,15 +197,12 @@ const Testimonials = () => {
                           ></path>
                         </svg>
                       </li> */}
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
